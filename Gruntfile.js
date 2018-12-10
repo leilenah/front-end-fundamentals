569150c023e889c74739f971a7d6b949eedc9642
mamel001 <leilenah.mamea@disney.com>
2018-12-09 16:06:52 -0800

    Set up sass files.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: { // TODO: Remove sourcemap
+                    style: 'expanded'
+                },
+                files: {
+                    'core-languages/css/animations.css': 'core-languages/css/sass/animations.scss',
+                    'core-languages/css/box-model.css': 'core-languages/css/sass/box-model.scss',
+                    'core-languages/css/css3.css': 'core-languages/css/sass/css3.scss',
+                    'core-languages/css/display.css': 'core-languages/css/sass/display.scss',
+                    'core-languages/css/fonts.css': 'core-languages/css/sass/fonts.scss',
+                    'core-languages/css/position.css': 'core-languages/css/sass/position.scss',
+                    'core-languages/css/queries.css': 'core-languages/css/sass/queries.scss',
+                    'core-languages/css/specificity.css': 'core-languages/css/sass/specificity.scss',
+                    'core-languages/css/transform.css': 'core-languages/css/sass/transform.scss',
+                    'core-languages/css/transitions.css': 'core-languages/css/sass/transitions.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass']);
+};
