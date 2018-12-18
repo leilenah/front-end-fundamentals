ea3dd96ed17e7c09bfef8bd8a7ab2e94e39bfc38
mamel001 <leilenah.mamea@disney.com>
2018-12-18 09:20:01 -0800

    Add form info.

--- front-end-languages/javascript/vanilla.js
+++ front-end-languages/javascript/vanilla.js
@@ -9,3 +9,7 @@ document.querySelector('.class-name')
 
 // query selector all -- get ALL matching elements in document
 document.querySelectorAll('.class-name')
+
+// get form value
+// <input type="text" name="name" id="uniqueID" value="value" />
+document.getElementById('uniqueID').value
