ba0817ef3f0a4dddc7de800db094ca46dd79547b
mamel001 <leilenah.mamea@disney.com>
2018-12-18 09:06:26 -0800

    Add query selector.

--- front-end-languages/javascript/vanilla.js
+++ front-end-languages/javascript/vanilla.js
@@ -1,3 +1,11 @@
 // get element by class name
+element.getElementsByClassName('class');
+
 // get element by id
-// query selector (2)
+element.getElementById('element-id');
+
+// query selector -- get FIRST matching element in document
+document.querySelector('.class-name')
+
+// query selector all -- get ALL matching elements in document
+document.querySelectorAll('.class-name')
