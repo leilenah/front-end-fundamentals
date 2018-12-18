9549bb966b52bc26ef12e24c49d1785bf69bfecf
mamel001 <leilenah.mamea@disney.com>
2018-12-18 00:00:50 -0800

    Add JavaScript events.

---
+++ front-end-languages/javascript/events.js
@@ -0,0 +1,15 @@
+/**
+ * jQuery Events
+ */
+
+$('.elements').on('click', function() {
+	// callback
+});
+
+/**
+ * Vanilla JS Events
+ */
+
+element.addEventListener('click', function() {
+	// callback
+});
